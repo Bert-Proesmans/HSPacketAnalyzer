9a2c0bcc364d180eb894522fe3cce04253390fa6
Bert Proesmans <bproesmans@hotmail.com>
2019-03-14 20:22:35 +0100

    Added mainwindow controls and debugconverter

---
+++ HSPacketAnalyzer/Views/Converters/DebugConverter.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace HSPacketAnalyzer.Views.Converters
+{
+	/// <summary>
+	/// Converter that can be used to break into the debugger for the purpose of
+	/// debugging bindings.
+	/// </summary>
+	/// <usage>Within your XAML code, bind like this; 
+	/// {Binding {}, Converter={StaticResource DebugConverter}}
+	/// </usage>
+	internal class DebugConverter : IValueConverter
+	{
+		public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			Debugger.Break();
+			return value;
+		}
+
+		public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			Debugger.Break();
+			return value;
+		}
+	}
+}
